a3e5363da63bd8cc7caebf8dbb21d597735d1bb5
Samreen Malik <160000739+devoleper-samreen@users.noreply.github.com>
2024-02-23 17:10:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// Add this script for handling button click and showing the form
+function showSignUpForm() {
+  var form = document.getElementById("signupForm");
+  form.classList.toggle("active");
+}
+
+console.log("js is included");
